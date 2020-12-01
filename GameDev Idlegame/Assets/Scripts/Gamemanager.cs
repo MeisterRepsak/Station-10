70a945e78143b70375b9dbfba2a582d11794bfe8
MeisterRepsak <71341417+MeisterRepsak@users.noreply.github.com>
2020-12-01 16:55:57 +0100

    Gay

---
+++ GameDev Idlegame/Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Gamemanager : MonoBehaviour
+{
+    private TextMeshProUGUI m_CashText;
+
+    public double m_Cash;
+
+   
+    void Start()
+    {
+        m_CashText = GameObject.Find("MoneyText").GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_CashText.text = m_Cash + "$";
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(!Physics.Raycast(ray, out hit) && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            m_Cash +=  1;
+        }
+        
+    }
+}
